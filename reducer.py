926c4110e55cc2ad6ae51e9496f910cd00a5077f
Van-Fokou <133280764+Van-Fokou@users.noreply.github.com>
2025-04-16 01:39:20 +0200

    Create reducer.py

---
+++ reducer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+
+max_word = None
+max_count = 0
+
+for line in sys.stdin:
+    try:
+        _, word, count = line.strip().split('\t')
+        count = int(count)
+        if count > max_count:
+            max_count = count
+            max_word = word
+    except ValueError:
+        continue
+
+if max_word:
+    print(f"{max_word}\t{max_count}")
